1943f72bd94c2539feabbcad7c694ec5c55eaafe
Alfredo.G.Marquez <Fredo-XVII@users.noreply.github.com>
2021-03-15 10:43:21 -0500

    pkg supporting docs

---
+++ NEWS.md
@@ -0,0 +1,3 @@
+# dis.sim.mod 0.0.0.9000
+
+* Added a `NEWS.md` file to track changes to the package.
---
+++ inst/pkg_dev.R
@@ -0,0 +1,14 @@
+# Package Development Script
+
+library(usethis)
+
+# Create package - done manually
+
+# Create supporting docs
+usethis::use_description()
+usethis::use_news_md()
+usethis::use_readme_rmd()
+usethis::use_gpl3_license(name = "Alfredo G Marquez")
+
+# Create functions
+
