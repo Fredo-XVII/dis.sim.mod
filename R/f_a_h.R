0680e81ef6a3af2254eba19cd12aff0c5098c94f
Alfredo.G.Marquez <Fredo-XVII@users.noreply.github.com>
2021-03-15 14:23:42 -0500

    with the first function

---
+++ R/f_a_h.R
@@ -0,0 +1,21 @@
+#' @title Centered Vector
+#'
+#' @description Create vector centered on `a` and bounded by `h`, by step size `step_size`.
+#'
+#' @details Creates a vector from a-h to a+h, centered on a, by step size `step_size`.
+#'
+#' @param a number, that centers the vector
+#' @param h number, the length from the center that is +/- from the center.
+#' @param step_size number, size of steps between numbers.
+#' @param ... arguments passed to `seq()` function.
+#'
+#' @return a vector from a-h to a+h, centered on a, by step size `step_size.`
+#'
+#' @examples {
+#' vector <- f_a_h(a = 0, h = 10, step_size = 1)
+#' }
+#'
+#' @export
+
+
+f_a_h <- function(a,h, step_size, ...) {seq(a - h, a + h, by = step_size, ...)}
